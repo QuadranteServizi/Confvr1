afc8ffa4a82b9abaaf94fa59a4e934e8250d0b59
QuadranteServizi <ced@quadranteservizi.it>
2014-03-05 14:58:46 +0100

    002

--- platforms/android/.staging/www/cordova_plugins.js
+++ platforms/android/.staging/www/cordova_plugins.js
@@ -142,13 +142,21 @@ module.exports = [
         "clobbers": [
             "window.FileTransfer"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.file": "1.0.0",
-    "org.apache.cordova.file-transfer": "0.4.1"
+    "org.apache.cordova.file": "1.0.1",
+    "org.apache.cordova.file-transfer": "0.4.2",
+    "org.apache.cordova.device": "0.2.8"
 }
 // BOTTOM OF METADATA
 });
